4f231a9824078c8ea0b9ba3e2812cf58f7c451f1
Kristin Yen <KristinYen@Kristins-MacBook-Pro.local>
2018-06-28 07:26:29 -0400

    Adding test to homepage

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -5,7 +5,7 @@ class Home extends React.Component{
     render(){
         return (
             <div className="container">
-                <h1>HOME PAGE</h1>
+                <h1>HOME PAGE TEST</h1>
             </div>
         );
     }
