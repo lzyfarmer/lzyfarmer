490fe2e9e673916678fe01dfc6bb8f8eb1c2604d
Kristin Yen <developlzyfarmer@gmail.com>
2018-06-29 22:32:10 -0400

    Added mlab

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 var express = require( "express" );
 var path = require( "path" );
+var bodyParser = require( "body-parser" );
+var mongodb = require( "mongodb" );
 var app = express();
-
 var port = process.env.PORT || 3000;
+var db;
 
 app.use( express.static( __dirname ) );
-
-/* eslint-disable no-console */
-console.log( "app", app );
 
 // Routes
 app.get( "*", function getAll( request, response ){
     response.sendFile( path.resolve( "./index.html" ) );
 } );
 
-// Setup
-app.listen( port, function listenServer(){
-    /* eslint-disable no-console */
-    console.log( "Listening on port 3000" );
+console.log( "MONGODB_URI", process.env.MONGODB_URI );
+
+mongodb.MongoClient.connect( process.env.MONGODB_URI, function mongoClientConnect( err, database ){
+  if( err ){
+    console.log( "err", err );
+    process.exit( 1 );
+  }
+
+  // Save database object from the callback for reuse.
+  db = database;
+  console.log( "Database connection ready" );
+
+  // Setup
+  app.listen( port );
 } );
