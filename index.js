5ed99450613fc72115fa02060f61eb9dce5cba9e
Kristin Yen <developlzyfarmer@gmail.com>
2018-06-29 22:55:51 -0400

    Working on mongodb

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ var path = require( "path" );
 var bodyParser = require( "body-parser" );
 var mongodb = require( "mongodb" );
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 var db;
 
 app.use( express.static( __dirname ) );
+app.use( bodyParser.json() );
 
 // Routes
 app.get( "*", function getAll( request, response ){
@@ -15,14 +16,14 @@ app.get( "*", function getAll( request, response ){
 
 console.log( "MONGODB_URI", process.env.MONGODB_URI );
 
-mongodb.MongoClient.connect( process.env.MONGODB_URI, function mongoClientConnect( err, database ){
+mongodb.MongoClient.connect( process.env.MONGODB_URI || "mongodb://localhost:27017/test", function mongoClientConnect( err, database ){
   if( err ){
     console.log( "err", err );
     process.exit( 1 );
   }
 
   // Save database object from the callback for reuse.
-  db = database;
+  db = database.db();
   console.log( "Database connection ready" );
 
   // Setup
