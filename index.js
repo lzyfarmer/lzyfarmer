aa770a0775bc19abbafe49e55f7b1774af31fbac
Kristin Yen <developlzyfarmer@gmail.com>
2018-07-26 20:29:56 -0400

    Removed code for https and whatever I was pulling in

--- index.js
+++ index.js
@@ -15,15 +15,6 @@ app.use( express.static( __dirname ) );
 app.use( bodyParser.json() );
 app.use( bodyParser.urlencoded( { "extended": true } ) );
 
-// Force into HTTPS if production
-// if( app.get( "env" ) === "production" ){
-//     app.use(function(req, res, next) {
-//         var protocol = req.get('x-forwarded-proto');
-//
-//         protocol == "https" ? next() : res.redirect( "https://" + req.hostname + req.url );
-//     } );
-// }
-
 mongoose.connect( process.env.MONGODB_URI, { "useMongoClient": true } );
 
 db = mongoose.connection;
