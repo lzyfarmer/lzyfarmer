e58dfdc1c68bcea1d3e01daf7d8c8d5c7dbd8f12
Kristin Yen <developlzyfarmer@gmail.com>
2018-07-31 16:26:18 -0400

    Fixed shop error

--- src/components/pages/createPlant/Upsell.js
+++ src/components/pages/createPlant/Upsell.js
@@ -4,7 +4,7 @@ import React from "react";
 var Upsell = ( props ) => (
     <div className="createPlantMenu">
         <p>Do you have all the materials needed for planting? You will need a container, soil, and seeds.</p>
-        <button className="light" onClick={ () => props.handleNavigate( "/store" ) }>Shop Now</button>
+        <button className="light" onClick={ () => { window.location = "https://amazon.com/plants"}>Shop Now</button>
     </div>
 );
 
